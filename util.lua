b4b3307bff290f9912ed3f619117b7336dab7f99
mittons <icyaxel@gmail.com>
2020-08-26 00:27:40 +0000

    Update util.lua

--- util.lua
+++ util.lua
@@ -7,10 +7,8 @@ function Util.get_blueprint(bp)
     if not (bp and bp.valid and bp.valid_for_read) then
         return nil
     end
-    if bp.is_blueprint_book and bp.active_index then
-		if not (bp.get_inventory(defines.inventory.item_main).is_empty()) then
-			return Util.get_blueprint(bp.get_inventory(defines.inventory.item_main)[bp.active_index])
-		end
+    if bp.is_blueprint_book and bp.active_index and (not bp.get_inventory(defines.inventory.item_main).is_empty()) then
+		return Util.get_blueprint(bp.get_inventory(defines.inventory.item_main)[bp.active_index])
     end
     if bp.is_blueprint then
         return bp
